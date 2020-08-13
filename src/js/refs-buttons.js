88754cb18be9235c6dfc8ecf878912fee3163b3c
Viktor Kirichok <viktor.kirichok@gmail.com>
2020-08-13 20:20:23 +0300

    Click on the button replace the search box with Watched and Queue buttons

---
+++ src/js/refs-buttons.js
@@ -0,0 +1,11 @@
+export default {
+	homeBg: document.querySelector('.section-top'),
+	libBg: document.querySelector('.section-top-lib'),
+	logoHome: document.querySelector('.logo-home'),
+	home: document.querySelector('.home'),
+	myLibrary: document.querySelector('.library-button'),
+	myWatched: document.querySelector('.watched-button'),
+	myQueue: document.querySelector('.queue-button'),
+	searchBox: document.querySelector('.search-block'),
+	error: document.querySelector('.error-search'),
+}
--- src/js/top-buttons.js
+++ src/js/top-buttons.js
@@ -1,38 +1,9 @@
-const refs = {
-	homeBg: document.querySelector('.section-top'),
-	libBg: document.querySelector('.section-top-lib'),
-	logoHome: document.querySelector('.logo-home'),
-	home: document.querySelector('.home'),
-	myLibrary: document.querySelector('.library-button'),
-	myWatched: document.querySelector('.watched-button'),
-	myQueue: document.querySelector('.queue-button'),
-	searchBox: document.querySelector('.search-block'),
-	error: document.querySelector('.error-search')
-}
+import refs from "./refs-buttons.js"
+import buttonFn from "./top-button-functions.js"
 
-refs.myLibrary.addEventListener('click', (event) => {
-	refs.searchBox.classList.add('visually-hidden');
-	refs.myLibrary.classList.add('visually-hidden');
-	refs.error.classList.remove('error-visible');
-	refs.myWatched.classList.remove('visually-hidden');
-	refs.myQueue.classList.remove('visually-hidden');
-	refs.homeBg.classList.replace('section-top', 'section-top-lib')
-})
 
-refs.home.addEventListener('click', (event) => {
-	refs.searchBox.classList.remove('visually-hidden');
-	refs.myLibrary.classList.remove('visually-hidden');
-	refs.error.classList.add('error-visible');
-	refs.myWatched.classList.add('visually-hidden');
-	refs.myQueue.classList.add('visually-hidden');
-	refs.homeBg.classList.replace('section-top-lib', 'section-top')
-})
-
-refs.logoHome.addEventListener('click', (event) => {
-	refs.searchBox.classList.remove('visually-hidden');
-	refs.myLibrary.classList.remove('visually-hidden');
-	refs.error.classList.add('error-visible');
-	refs.myWatched.classList.add('visually-hidden');
-	refs.myQueue.classList.add('visually-hidden');
-	refs.homeBg.classList.replace('section-top-lib', 'section-top')
-})
+refs.myLibrary.addEventListener('click', buttonFn.openMyLibrary);
+refs.home.addEventListener('click', buttonFn.closeMyLibrary);
+refs.logoHome.addEventListener('click', buttonFn.closeMyLibrary);
+refs.myWatched.addEventListener('click', buttonFn.openWatched);
+refs.myQueue.addEventListener('click', buttonFn.openQueue);
