5a09809fcbfd1efd7ee08eaf4e277750070d7720
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-08-16 02:56:59 +0300

    fix buttons active in header

--- src/js/top-button-functions.js
+++ src/js/top-button-functions.js
@@ -1,34 +1,36 @@
-import refs from "./refs-buttons.js"
-
+import refs from './refs-buttons.js';
 
 const openMyLibrary = () => {
-	refs.searchBox.classList.add('visually-hidden');
-	refs.error.classList.remove('error-visible');
-	refs.myWatched.classList.remove('visually-hidden');
-	refs.myQueue.classList.remove('visually-hidden');
-	refs.myWatched.classList.add('active');
-	refs.myQueue.classList.remove('active');
-	refs.homeBg.classList.replace('section-top', 'section-top-lib')
+  refs.searchBox.classList.add('visually-hidden');
+  refs.error.classList.remove('error-visible');
+  refs.myWatched.classList.remove('visually-hidden');
+  refs.myQueue.classList.remove('visually-hidden');
+  refs.myWatched.classList.add('active');
+  refs.myQueue.classList.remove('active');
+  refs.homeBg.classList.add('section-top-lib');
+  refs.linkHome.classList.remove('active-menu');
+  refs.linkLib.classList.add('active-menu');
 };
 
 const closeMyLibrary = () => {
-	refs.searchBox.classList.remove('visually-hidden');
-	refs.error.classList.add('error-visible');
-	refs.myWatched.classList.add('visually-hidden');
-	refs.myQueue.classList.add('visually-hidden');
-	refs.homeBg.classList.replace('section-top-lib', 'section-top')
-	refs.myWatched.classList.remove('active');
-	refs.myQueue.classList.remove('active');
+  refs.searchBox.classList.remove('visually-hidden');
+  refs.myWatched.classList.add('visually-hidden');
+  refs.myQueue.classList.add('visually-hidden');
+  refs.homeBg.classList.remove('section-top-lib');
+  refs.myWatched.classList.remove('active');
+  refs.myQueue.classList.remove('active');
+  refs.linkLib.classList.remove('active-menu');
+  refs.linkHome.classList.add('active-menu');
 };
 
 const openWatched = () => {
-	refs.myWatched.classList.add('active');
-	refs.myQueue.classList.remove('active');
+  refs.myWatched.classList.add('active');
+  refs.myQueue.classList.remove('active');
 };
 
 const openQueue = () => {
-	refs.myWatched.classList.remove('active');
-	refs.myQueue.classList.add('active');
+  refs.myWatched.classList.remove('active');
+  refs.myQueue.classList.add('active');
 };
 
-export default { openMyLibrary, closeMyLibrary, openWatched, openQueue }
+export default { openMyLibrary, closeMyLibrary, openWatched, openQueue };
