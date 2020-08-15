ce2f68232e398b58d254eabfe9c94be15d3395b7
Maksim Skipin <skipysmaks@gmail.com>
2020-08-15 11:20:20 +0300

    added active class name to navigation item

--- src/js/modal-authorization.js
+++ src/js/modal-authorization.js
@@ -10,11 +10,13 @@ document.querySelector('.btn-login').onclick = () => {
         class="login-block__user-name"
         type="text"
         placeholder="Enter your name..."
+        required
       />
       <input
         class="login-block__password"
         type="password"
         placeholder="Enter your password..."
+        required
       />
       <div class="login-block__btn-block">
         <button class="login-block__log-in btn-default">Log in</button>
