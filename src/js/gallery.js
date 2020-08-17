1e670311605d83061f0277dda6adc971f38130d2
Nick <nikitakrm87@gmail.com>
2020-08-17 20:03:38 +0300

    pagination start/search featch

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -19,7 +19,6 @@ startFetch().then(data => {
 // on submit
 refs.form.addEventListener('submit', event => {
   event.preventDefault();
-
   refs.activeMenu.classList.remove('active-menu');
 
   clearPage();
@@ -35,6 +34,7 @@ refs.form.addEventListener('submit', event => {
     inputValue = event.currentTarget.elements.search.value;
 
     searchFetch(inputValue).then(data => {
+      console.log(inputValue);
       if (data.results.length === 0) {
         document.querySelector('#pagination').classList.add('not-visible');
 
@@ -62,11 +62,15 @@ function renderWithTimeout(data, currentPage) {
 
     paginatRef.classList.remove('not-visible');
 
-    paginatRef.addEventListener('click', handlePagination);
+    if (inputValue) {
+      paginatRef.addEventListener('click', handleSearchPagination);
+    } else {
+      paginatRef.addEventListener('click', handlePagination);
+    };
 
     refs.spinnerLoader.classList.add('not-visible');
-  }, 1500);
-}
+  }, 1000);
+};
 
 function handlePagination(event) {
   const currentPageNum = Number(
@@ -85,14 +89,43 @@ function handlePagination(event) {
     startFetch(changeNumPage('minus', currentPageNum)).then(data => {
       clearPage();
       renderWithTimeout(data, currentPageNum);
-      console.log(data);
+      // console.log(data);
     });
   } else if (event.target.classList.contains('arrow-right')) {
     // console.log('kuku right');
     startFetch(changeNumPage('plus', currentPageNum)).then(data => {
       clearPage();
       renderWithTimeout(data, currentPageNum);
-      console.log(data);
+      // console.log(data);
+    });
+  }
+}
+
+function handleSearchPagination(event) {
+  const currentPageNum = Number(
+    event.currentTarget.querySelector('.current').textContent,
+  );
+
+  if (event.target.textContent !== '') {
+    const targetNum = event.target.textContent;
+    document.querySelector('#pagination').classList.add('not-visible');
+
+    searchFetch(inputValue, targetNum).then(data => {
+      clearPage();
+      renderWithTimeout(data, Number(targetNum));
+    });
+  } else if (event.target.classList.contains('arrow-left')) {
+    searchFetch(inputValue, changeNumPage('minus', currentPageNum)).then(data => {
+      clearPage();
+      renderWithTimeout(data, currentPageNum);
+      // console.log(data);
+    });
+  } else if (event.target.classList.contains('arrow-right')) {
+    // console.log('kuku right');
+    searchFetch(inputValue, changeNumPage('plus', currentPageNum)).then(data => {
+      clearPage();
+      renderWithTimeout(data, currentPageNum);
+      // console.log(data);
     });
   }
 }
