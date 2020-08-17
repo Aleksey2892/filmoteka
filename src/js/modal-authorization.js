a88438e789adfa36ad5f4737d5bfb84e18c3e8fc
Maksim Skipin <skipysmaks@gmail.com>
2020-08-17 20:51:04 +0300

    added notification on log in

--- src/js/modal-authorization.js
+++ src/js/modal-authorization.js
@@ -1,7 +1,7 @@
 import * as basicLightbox from 'basiclightbox';
 import 'basiclightbox/src/styles/main.scss';
 import registration from './registration';
-import addOnlineUser from './addOnlineUser';
+import logIn from './logIn';
 
 const testBox = basicLightbox;
 
@@ -84,24 +84,3 @@ document.querySelector('.btn-login').onclick = () => {
 
   document.querySelector('.login-block').addEventListener('submit', logIn);
 };
-
-function logIn() {
-  event.preventDefault();
-
-  const localStorageData = JSON.parse(localStorage.getItem('users'));
-  let userName = document.querySelector('.login-block__user-name').value;
-  const userPass = document.querySelector('.login-block__password').value;
-
-  userName = userName.toLowerCase();
-
-  localStorageData.find(el => {
-    if (userName === el.userName && userPass === el.userPass) {
-      addOnlineUser(userName);
-      el.isOnline = true;
-      console.log('ку-ку епта, ты зашел!!!');
-    }
-
-    localStorage.setItem('users', JSON.stringify(localStorageData));
-    localStorage.setItem('isOnline', JSON.stringify(userName));
-  });
-}
