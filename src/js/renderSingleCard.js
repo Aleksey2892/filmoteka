5db249056f2d4e2cee37425253d3f52d02718606
tamara-f <filatovatam@ukr.net>
2020-08-18 15:33:57 +0300

    header for details

--- src/js/renderSingleCard.js
+++ src/js/renderSingleCard.js
@@ -4,15 +4,19 @@ import onefilmCard from '../templates/onefilmCard.hbs';
 import clearPage from './clearPage';
 
 refs.listFilms.addEventListener('click', event => {
-    //   console.log(event.currentTarget); //ul
-    // console.dir(event.target); //img, h3, ..
+  //   console.log(event.currentTarget); //ul
+  // console.dir(event.target); //img, h3, ..
 
-    if (event.target.nodeName === 'IMG' || event.target.nodeName === 'H3') {
-        // console.log(event.target.dataset.id);
-        cardFetch(event.target.dataset.id).then(obj => {
-            clearPage();
-            refs.pagination.classList.add('not-visible');
-            refs.listFilms.insertAdjacentHTML('beforeend', onefilmCard(obj));
-        });
-    }
+  if (event.target.nodeName === 'IMG' || event.target.nodeName === 'H3') {
+    cardFetch(event.target.dataset.id).then(obj => {
+      clearPage();
+
+      const headerBg = document.querySelector('.head-background');
+      headerBg.classList.remove('section-top');
+      headerBg.classList.add('section-top-details');
+
+      refs.pagination.classList.add('not-visible');
+      refs.listFilms.insertAdjacentHTML('beforeend', onefilmCard(obj));
+    });
+  }
 });
