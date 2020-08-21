dde3724d665a1835b5faa8ba79f85a3f84072b1f
Aleksey Grygorenko <aleksey2892@gmail.com>
2020-08-21 18:57:07 +0300

    delete all

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './styles.scss';
-import './js/defaultLoadLocal';
-import './js/modal-authorization';
-import './js/logOut';
-import './js/gallery';
-import './js/oneCardFetch';
-import './js/renderSingleCard';
-import './js/top-buttons.js';
-import './js/paginationLogic';
--- src/js/clearPage.js
+++
@@ -1,5 +0,0 @@
-import refs from './refs';
-
-export default function clearPage() {
-  refs.listFilms.innerHTML = '';
-}
